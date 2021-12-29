e91040f13e5aebe75d8ff50baa9e666b5f3862e4
Ivanshelemeh <neo041990@mail.ru>
2021-12-29 12:54:36 +0300

    refactored RestIssueTest.java

---
+++ src/main/resources/js/shelePlugin.js
@@ -0,0 +1,41 @@
+AJS.toInit(function () {
+    console.log("ddfd");
+    AJS.$('#valid').hide();
+    AJS.$('#notify').hide();
+    AJS.$('#form-1').submit(function (event) {
+        AJS.$('#showAction').hide();
+        event.preventDefault();
+        let projects = $('#select2-example').val();
+        if (projects === null) {
+            AJS.$('#valid').show();
+            AJS.$('#notify').hide();
+        }
+        let events = $('#event').val();
+        if (events === null) {
+            AJS.$('#valid').show();
+            AJS.$('#notify').hide();
+        }
+
+        const settings = {
+            project: projects,
+            eventTypeIds: events
+        }
+
+        const request = jQuery.ajax({
+            type: "POST",
+            data: JSON.stringify(settings),
+            url: AJS.contextPath() + "/rest/rest-issue/1.0/api/issue",
+            headers: {'X-Atlassian-Token': 'nocheck'},
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            statusCode: {
+                201: function () {
+                    AJS.$('#notify').show();
+                    if (projects === null || events === null) {
+                        AJS.$('#notify').hide();
+                    }
+                }
+            }
+        });
+    });
+});
--- src/test/java/com/atlassian/shele/shelePlugin/ao/test/RestIssueTest.java
+++ src/test/java/com/atlassian/shele/shelePlugin/ao/test/RestIssueTest.java
@@ -2,14 +2,16 @@ package com.atlassian.shele.shelePlugin.ao.test;
 
 import com.atlassian.shele.shelePlugin.ao.ProjectDTO;
 import com.atlassian.shele.shelePlugin.ao.ProjectEntityService;
+import com.atlassian.shele.shelePlugin.rest.RestIssueService;
 import com.google.common.collect.ImmutableList;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -17,10 +19,13 @@ import static org.junit.Assert.assertEquals;
 /**
  * Unit test via mock of ProjectEntityService
  */
+@RunWith(MockitoJUnitRunner.class)
 public class RestIssueTest {
 
-    @Mock
-    private ProjectEntityService service;
+    private ProjectEntityService projectEntityService = Mockito.mock(ProjectEntityService.class);
+
+    @InjectMocks
+    private RestIssueService restIssueService = new RestIssueService(projectEntityService);
 
     @Before
     public void setUp() throws Exception {
@@ -31,7 +36,7 @@ public class RestIssueTest {
     public void should_be_Equals_DTO_Entity() {
         ProjectDTO projectDTO = new ProjectDTO();
         projectDTO.setProject(ImmutableList.of("project", "new wall"));
-        projectDTO.setEventTypeIds(ImmutableList.of(1l, 2L, 3L));
+        projectDTO.setEventTypeIds(ImmutableList.of(1L, 2L, 3L));
         List<String> projectDTOList = projectDTO.getProject();
         assertEquals(projectDTOList, projectDTO.getProject());
     }
@@ -39,8 +44,10 @@ public class RestIssueTest {
     @Test
     public void verify_ProjectDTO_test() {
         ProjectDTO projectDTO = new ProjectDTO();
-        List<ProjectDTO> projectDTOCollection = new ArrayList<>();
-        Mockito.when(service.saveEntity(projectDTO)).thenReturn((projectDTOCollection));
-        Mockito.verify(Mockito.mock(ProjectEntityService.class), Mockito.atLeastOnce());
+        projectDTO.setProject(ImmutableList.of("some project"));
+        projectDTO.setEventTypeIds(ImmutableList.of(2L, 4L));
+        List<ProjectDTO> projectDTOCollection = projectEntityService.saveEntity(projectDTO);
+        Mockito.when(projectEntityService.saveEntity(projectDTO)).thenReturn((projectDTOCollection));
+        Mockito.verify(projectEntityService, Mockito.times(1)).saveEntity(Mockito.anyObject());
     }
 }
